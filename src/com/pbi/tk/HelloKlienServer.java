8a191f398add4205b5e6bf679db4c98b2f0c692f
AnandaRiska <anandakhoiriah8@gmail.com>
2026-09-19 11:10:44 +0700

    Praktikum 1 - proyek Java pertama

---
+++ src/com/pbi/tk/HelloKlienServer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pbi.tk;
+
+/**
+ *
+ * @author ACER
+ */
+public class HelloKlienServer {
+    
+    public static void main(String[] args) {
+        System.out.println("==============================");
+        System.out.println("Nama  : Ananda Riska Khoiriah");
+        System.out.println("NIM   : 25110203");
+        System.out.println("Kelas : TK Grup C");
+        System.out.println("==============================");
+        System.out.println("Aplikasi klien siap dijalankan");
+    }
+}
